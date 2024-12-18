261234bad08c8055c5d4e4457fdf970090744ba8
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2024-12-18 15:02:13 +0900

    mysite02 #1

--- guestbook02/src/main/java/guestbook/dao/GuestbookDao.java
+++ guestbook02/src/main/java/guestbook/dao/GuestbookDao.java
@@ -17,7 +17,7 @@ public class GuestbookDao {
 		
 		try (
 			Connection conn = getConnection();
-			PreparedStatement pstmt = conn.prepareStatement("select id, name, contents, date_format('%Y-%m-%d %h:%i:%s', reg_date) from guestbook order by reg_date desc");	
+			PreparedStatement pstmt = conn.prepareStatement("select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc");	
 			ResultSet rs = pstmt.executeQuery();
 		) {
 			while(rs.next()) {
@@ -54,5 +54,42 @@ public class GuestbookDao {
 		} 
 		
 		return conn;
+	}
+
+	public int insert(GuestbookVo vo) {
+		int count = 0;
+		
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("insert into guestbook values(null, ?, ?, ?, now())");
+		) {
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getContents());
+			
+			count = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} 
+		
+		return count;		
+	}
+
+	public int deleteByIdAndPassword(Long id, String password) {
+		int count = 0;
+		
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("delete from guestbook where id=? and password=?");
+		) {
+			pstmt.setLong(1, id);
+			pstmt.setString(2, password);
+			
+			count = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} 
+		
+		return count;		
 	}	
 }
