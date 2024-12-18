e82c0543cb218d4c33209149395fbc2bf735f9b1
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2024-12-18 10:54:25 +0900

    guestbook02 #2

---
+++ guestbook02/src/main/java/guestbook/controller/GuestbookServlet.java
@@ -0,0 +1,31 @@
+package guestbook.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import guestbook.dao.GuestbookDao;
+import guestbook.vo.GuestbookVo;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/gb")
+public class GuestbookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("a");
+		if("add".equals(action)) {
+			
+		} else {
+			List<GuestbookVo> list = new GuestbookDao().findAll();
+			System.out.println(list);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
