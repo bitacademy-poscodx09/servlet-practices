7ceca55f5cdb73057b8a3d45485e42eed3116994
bitacademy-poscodx09 <bitacademy.poscodx@gmail.com>
2024-12-19 15:54:36 +0900

    sql updated

--- guestbook02/db/query.sql
+++ guestbook02/db/query.sql
@@ -7,4 +7,5 @@ insert into guestbook values(null, '둘리', '1234', 'ㅎㅇ!', now());
 select id, name, contents, date_format(reg_date, '%Y-%m-%d %h:%i:%s') from guestbook order by reg_date desc;
 
 -- delete
-
+delete from guestbook where id=1 and password='1234';
+select * from guestbook;
---
+++ guestbook02/db/user.sql
@@ -0,0 +1,4 @@
+select * from user;
+
+-- login
+select id, name from user where email='dooly@gmail.com' and password='1234';
